e5a5c660c186c2a1b878bbd0ca6bed3400b94511
Aditya Sarin <aditya28sarin@gmail.com>
2021-02-08 00:48:33 +0530

    Complteted SignUp with virtual to save hashed passowrds and also write logic for signup controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,8 +1,32 @@
+const User = require('../models/user');
+const shortId = require('shortid'); //used to generate random user id when a user signs up 
+
 exports.signup = (req,res) => {
 
-    const {name, email, password} = req.body;
+    User.findOne({email:req.body.email}).exec((err,user) => {
+        if(user){
+            return res.status(400).json({
+                error: 'Email is taken'
+            })
+        }
 
-    res.json({
-        user: {name, email, password}
+        const {name,email,password} = req.body;
+        let username = shortId.generate();
+        let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+        let newUser = new User({name, email, password, profile, username})
+        newUser.save((err,success)=>{
+            if(err){
+                return res.status(400).json({
+                    error:err
+                })
+            }
+            // res.json({
+            //     user:success
+            // })
+            res.json({
+                message: 'SignUp Success! Please SignIn.'
+            })
+        });
     })
 };
--- backend/models/user.js
+++ backend/models/user.js
@@ -62,4 +62,46 @@ const userSchema =  new mongoose.Schema({
 }, {timestamp:true});
 
 
+userSchema.virtual('password')
+.set(function(password){
+    //create a temporary variable called _password
+
+    this._password = password
+
+    //generate a 'salt' used for hashing algo 
+
+    this.salt = this.makeSalt()
+
+    //encrypt password
+
+    this.hashed_password = this.encryptPassword(password)
+})
+.get(function(){
+    return this._password;
+});
+
+
+userSchema.methods = {
+
+    //will be used during the user signin
+    authenticate: function(plainText){
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
+
+    encryptPassword: function(password){
+        if(!password) return '';
+        try{
+           return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+        } catch(err){
+            return '';
+        }
+    },
+
+    makeSalt: function(){
+        return Math.round(new Date().valueOf() * Math.random()) + '';
+    }
+};
+
+
 module.exports = mongoose.model('User', userSchema);
